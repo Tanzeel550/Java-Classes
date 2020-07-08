b760434047fc5393f754cb0e5448ac562b9d4a85
Tanzeel550 <tanzeelahmed550@gmail.com>
2020-07-08 12:09:46 +0500

    added LinkedList

--- myLinkedList/src/tanzeel_LinkedList/Main.java
+++ myLinkedList/src/tanzeel_LinkedList/Main.java
@@ -4,23 +4,24 @@ package tanzeel_LinkedList;
 public class Main {
 	
 	public static void main(String args[]) {
-		MyLinkedList ll = new MyLinkedList();
+		MyLinkedList<Integer> ll = new MyLinkedList<>();
 		ll.add(10);
 		ll.add(11);
 		ll.add(12);
 		ll.add(12);
 		ll.add(12);
 		
+		Integer o = 12;
 		
-//		System.out.println(ll.size());
-//		System.out.println(ll.isEmpty());
-//		System.out.println(ll.contains(11));
-//		System.out.println(ll.remove(10));
-//		System.out.println(ll.get(2));
-//		System.out.println(ll.set(1, 1000));
-//		System.out.println(ll.indexOf(11));
-//		ll.removeFirst();
-//		ll.print();
+		System.out.println(ll.size());
+		System.out.println(ll.isEmpty());
+		System.out.println(ll.contains(11));
+		System.out.println(ll.removeAll(o));
+		System.out.println(ll.get(2));
+		System.out.println(ll.set(1, 1000));
+		System.out.println(ll.indexOf(11));
+		ll.removeFirst();
+		ll.print();
 		
 		System.out.println(ll.lastIndexOf(12));
 		
--- myLinkedList/src/tanzeel_LinkedList/MyLinkedList.java
+++ myLinkedList/src/tanzeel_LinkedList/MyLinkedList.java
@@ -1,21 +1,20 @@
 package tanzeel_LinkedList;
 
-
-public class MyLinkedList{
+public class MyLinkedList<E> {
 
 	Node head;
 	
-	static class Node{
-		int data;
+	class Node{
+		E data;
 		Node next;
 		
-		Node(int e){
+		Node(E e){
 			this.data = e;
 			this.next = null;
 		}
 	}
 	
-	public MyLinkedList(int data){
+	public MyLinkedList(E data){
 		Node to_add = new Node(data);
 		this.head = to_add;
 	}
@@ -80,7 +79,7 @@ public class MyLinkedList{
 	}
 
 //	Done
-	public boolean contains(int o) {
+	public boolean contains(E o) {
 		if(this.head == null)
 			return false;
 		
@@ -100,25 +99,30 @@ public class MyLinkedList{
 
 
 //	Done
-	public int[] toArray() {
-		if(this.head == null)
-			return null;
-		
-		int arr[] = new int[this.size()];
-		int pos = 0;
-		Node curr_node = this.head;
-		
-		while(curr_node.next != null) {
-			arr[pos] = curr_node.data;
-			curr_node = curr_node.next;
-			pos++;
-		}
-		arr[pos] = curr_node.data;
-		return arr;
-	}
+	/**
+	 * 
+	 * works only for int type i.e without using generic classes
+	 */
+//	public E[] toArray() {
+//	
+//		if(this.head == null)
+//			return null;
+//		
+//		E[] arr = new E[this.size()];
+//		int pos = 0;
+//		Node curr_node = this.head;
+//		
+//		while(curr_node.next != null) {
+//			arr[pos] = curr_node.data;
+//			curr_node = curr_node.next;
+//			pos++;
+//		}
+//		arr[pos] = curr_node.data;
+//		return arr;
+//	}
 
 //	Done
-	public boolean add(int e) {
+	public boolean add(E e) {
 		Node to_add = new Node(e);
 		if(head == null) {
 			this.head = to_add;
@@ -133,7 +137,35 @@ public class MyLinkedList{
 		return true;
 	}
 
-//	public boolean remove(int index)
+	public boolean remove(int index) {
+		
+		if(this.head == null)
+			return false;
+		
+		if(head.next == null) {
+			if(index == 1) {
+				this.head = null;
+				return true;
+			}
+			return false;
+		}
+		
+		if(index == 0)
+			this.head.next = this.head.next.next;
+		
+		Node curr_node = this.head;
+		int pos = 0;
+		while(curr_node.next != null) {
+			if(pos == index-1) {
+				curr_node.next = curr_node.next.next;
+				return true;
+			}
+			curr_node = curr_node.next;
+			pos++;
+		}
+		System.out.println("Index out of range.");
+		return false;
+	}
 	
 	public boolean removeFirst() {
 		
@@ -147,7 +179,34 @@ public class MyLinkedList{
 	}
 	
 	
-	public boolean remove(int o){
+	public boolean remove(E o){
+		
+		if(this.head == null){ 
+			System.out.println("You are trying to remove from LinkedList which is Empty.");
+			return false;
+		}
+		
+		boolean isRemoved = false;
+		Node curr_node = this.head;
+		
+		if(curr_node.data == o) {
+			this.removeFirst();
+			return true;
+		}
+		
+		while(curr_node.next != null) {
+			if(curr_node.next.data == o) {
+				curr_node.next = curr_node.next.next;
+				isRemoved = true;
+				break;
+			}
+			curr_node = curr_node.next;
+		}
+		
+		return isRemoved;
+	}
+	
+public boolean removeAll(E o){
 		
 		if(this.head == null){ 
 			System.out.println("You are trying to remove from LinkedList which is Empty.");
@@ -182,9 +241,9 @@ public class MyLinkedList{
 	}
 
 	
-	public int get(int index) {
+	public E get(int index) {
 		if(head == null)
-			return -1;
+			return null;
 		
 		if(head.next == null)
 			return head.data;
@@ -200,11 +259,11 @@ public class MyLinkedList{
 			curr_node = curr_node.next;
 		}
 		System.out.println("Index out of range.");
-		return -1;
+		return null;
 	}
 
 //	Done
-	public boolean set(int index, int data) {
+	public boolean set(int index, E data) {
 		if(head == null)
 			return false;
 		
@@ -228,7 +287,7 @@ public class MyLinkedList{
 	}
 	
 //	Done
-	public int indexOf(int o) {
+	public int indexOf(E o) {
 		
 		int curr_index= 0;
 		
@@ -244,7 +303,7 @@ public class MyLinkedList{
 	}
 	
 //	Done
-	public int lastIndexOf(int o) {
+	public int lastIndexOf(E o) {
 		if(this.head == null)
 			return -1;
 		
